e5f1e044190dfa34b0b4007168db3a6a0b5b22f0
Joshua C. Forest <josh.forest@rstudio.com>
2021-05-17 20:25:55 -0400

    switch to I2C display type, and programmatically assign button things

--- multi_button/multi_button.ino
+++ multi_button/multi_button.ino
@@ -7,52 +7,43 @@
  */
 
 // include the library code:
-#include <LiquidCrystal.h>
 #include <Servo.h>
+#include "SSD1306Ascii.h"
+#include "SSD1306AsciiAvrI2c.h"
 
+#define I2C_ADDRESS 0x3C
 #define ButtonsPressed 9
+#define NumButtons 10
 
-// button pins
-const int b1 = A1, b2 = A2, b3 = A3, b4 = A4, b5 = A5, b6 = 6, b7 = 5, b8 = 4, b9 = 3, b10 = 2;
 // the lock pin
 const int l1 = 13;
 // base sleep time in ms
 const int sleep = 1000;
-// LCD pins
-const int rs = 12, en = 11, d4 = 10, d5 = 9, d6 = 8, d7 = 7;
 // locked position of servo
 const int lockedPos = 90;
 // unlocked position of servo
 const int unlockedPos = 180;
 
-LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+SSD1306AsciiAvrI2c oled;
 Servo lock;
 
-// Button states
-int bs1 = 0, bs2 = 0, bs3 = 0, bs4 = 0, bs5 = 0, bs6 = 0, bs7 = 0, bs8 = 0, bs9 = 0, bs10 = 0;
-// locked or not
+int buttons[] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11}; // button pins
+int buttonStates[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+char buttonValues[] = {'1', '2', '3', '4', '5', '6', '7', '8', '9', '0'};
 bool locked = true;
-// array of buttons pressed
-char Data[ButtonsPressed];
-// button presses to unlock
-char Master[ButtonsPressed] = "01181903";
-// number of buttons pressed so far
-byte dataCount = 0;
+char Data[ButtonsPressed]; // array of buttons pressed
+char Master[ButtonsPressed] = "01181903"; // button presses to unlock
+byte dataCount = 0; // number of buttons pressed so far
 
 void setup() {
-  // set up the LCD's number of columns and rows
-  lcd.begin(16, 2);
+  // set up the display
+  oled.begin(&Adafruit128x64, I2C_ADDRESS);
+  oled.setFont(ZevvPeep8x16);
+  oled.clear();
   // initialize the pushbutton pins as an input
-  pinMode(b1, INPUT);
-  pinMode(b2, INPUT);
-  pinMode(b3, INPUT);
-  pinMode(b4, INPUT);
-  pinMode(b5, INPUT);
-  pinMode(b6, INPUT);
-  pinMode(b7, INPUT);
-  pinMode(b8, INPUT);
-  pinMode(b9, INPUT);
-  pinMode(b10, INPUT);
+  for (int i = 0; i < NumButtons; i++) {
+    pinMode(i, INPUT);
+  }
   lock.attach(l1);  // attaches the servo on the l1 pin to the servo object
   lockContainer();
   pushButtons("");
@@ -82,18 +73,19 @@ void lockContainer() {
 }
 
 void pushButtons(String message) {
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print("Push buttons!");
-  lcd.setCursor(0, 1);
-  lcd.print(message);
+  // Output the main push buttons text, with additional message line
+  oled.clear();
+  oled.setCursor(0, 0);
+  oled.print("Push buttons!");
+  oled.setCursor(0, 2);
+  oled.print(message);
 }
 
 // Store the button press in the Data array
 void storeButtonPress(char button) {
   Data[dataCount] = button;
-  lcd.setCursor(dataCount, 1);
-  lcd.print(String(Data[dataCount]));
+  oled.setCursor(dataCount*8, 2);
+  oled.print(String(Data[dataCount]));
   dataCount++;
   delay(sleep/4);
 }
@@ -107,16 +99,9 @@ void clearData() {
 }
 
 void readButtons() {
-  bs1 = digitalRead(b1);
-  bs2 = digitalRead(b2);
-  bs3 = digitalRead(b3);
-  bs4 = digitalRead(b4);
-  bs5 = digitalRead(b5);
-  bs6 = digitalRead(b6);
-  bs7 = digitalRead(b7);
-  bs8 = digitalRead(b8);
-  bs9 = digitalRead(b9);
-  bs10 = digitalRead(b10);
+  for (int i = 0; i < NumButtons; i++) {
+    buttonStates[i] = digitalRead(buttons[i]);
+  }
 }
 
 void buttonPuzzle() {
@@ -124,36 +109,12 @@ void buttonPuzzle() {
   readButtons();
 
   if (locked) {
-    if (bs1 == HIGH) {
-      storeButtonPress('1');
+    for (int i = 0; i < NumButtons; i++) {
+      if (buttonStates[i] == HIGH) {
+        storeButtonPress(buttonValues[i]);
+      }
     }
-    else if (bs2 == HIGH) {
-      storeButtonPress('2');
-    }
-    else if (bs3 == HIGH) {
-      storeButtonPress('3');
-    }
-    else if (bs4 == HIGH) {
-      storeButtonPress('4');
-    }
-    else if (bs5 == HIGH) {
-      storeButtonPress('5');
-    }
-    else if (bs6 == HIGH) {
-      storeButtonPress('6');
-    }
-    else if (bs7 == HIGH) {
-      storeButtonPress('7');
-    }
-    else if (bs8 == HIGH) {
-      storeButtonPress('8');
-    }
-    else if (bs9 == HIGH) {
-      storeButtonPress('9');
-    }
-    else if (bs10 == HIGH) {
-      storeButtonPress('0');
-    }
+
 
     if (dataCount == ButtonsPressed - 1) {
       if (!strcmp(Data, Master)) {
@@ -161,6 +122,7 @@ void buttonPuzzle() {
         unlockContainer();
         delay(sleep*2);
         clearData();
+        oled.clear();
       } else {
         pushButtons("INCORRECT!");
         delay(sleep*2);
@@ -168,16 +130,18 @@ void buttonPuzzle() {
       }
     }
   } else {
-    lcd.setCursor(0, 0);
-    lcd.write("Push any button");
-    lcd.setCursor(0, 1);
-    lcd.write("to lock the box!");
+    oled.setCursor(0, 2);
+    oled.write("Push any button");
+    oled.setCursor(0, 4);
+    oled.write("to lock the box!");
 
     // read the state of the pushbutton values:
     readButtons();
 
-    if ((bs1 || bs2 || bs3 || bs4 || bs5 || bs6 || bs7 || bs8 || bs9 || bs10) == HIGH) {
-      lockContainer();
+    for (int i = 0; i < NumButtons; i++) {
+      if (buttonStates[i] == HIGH) {
+        lockContainer();
+      }
     }
   }
 }
