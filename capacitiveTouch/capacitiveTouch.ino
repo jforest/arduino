7fc611b7a8db5f8c543a0b41d49d6ad391231688
Joshua C. Forest <josh.forest@rstudio.com>
2021-05-17 20:27:19 -0400

    switch to I2C display type

--- capacitiveTouch/capacitiveTouch.ino
+++ capacitiveTouch/capacitiveTouch.ino
@@ -1,4 +1,7 @@
-#include <LiquidCrystal.h>
+#include "SSD1306Ascii.h"
+#include "SSD1306AsciiAvrI2c.h"
+
+#define I2C_ADDRESS 0x3C
 
 // this constant won't change. It's the pin number of the sensor's output:
 const int blue = A0;
@@ -6,21 +9,22 @@ const int green = A1;
 const int red = A2;
 const int yellow = A3;
 const int white = A4;
-const int rs = 52, en = 53, d4 = 51, d5 = 50, d6 = 49, d7 = 48;
 
-LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+SSD1306AsciiAvrI2c oled;
 
 // wire states
 int blueState = 0, greenState = 0, redState = 0, yellowState = 0, whiteState = 0;
 
 void setup() {
-  lcd.begin(16, 2);
+  oled.begin(&Adafruit128x64, I2C_ADDRESS);
+  oled.setFont(ZevvPeep8x16);
+  oled.clear();
   pinMode(blue, INPUT);
   pinMode(green, INPUT);
   pinMode(red, INPUT);
   pinMode(yellow, INPUT);
   pinMode(white, INPUT);
-  lcd.print("Touch Wires!");
+  touchWires("");
 }
 
 void loop() {
@@ -64,9 +68,9 @@ void readTouches() {
 
 void touchWires(String message) {
   // Output the main push buttons text, with additional message line
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print("Touch Wires!");
-  lcd.setCursor(0, 1);
-  lcd.print(message);
+  oled.clear();
+  oled.setCursor(0, 0);
+  oled.print("Touch Wires!");
+  oled.setCursor(0, 2);
+  oled.print(message);
 }
