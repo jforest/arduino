1985ad12d1bdb4ef9e5b1a146ce7a88ef846439d
Joshua C. Forest <josh.forest@rstudio.com>
2021-05-21 10:12:35 -0400

    switch to arduino nano, so update pins, and use wire.  Also remove unneded function

--- morseCodeTransmitter/morseCodeTransmitter.ino
+++ morseCodeTransmitter/morseCodeTransmitter.ino
@@ -1,6 +1,7 @@
 // Morse code - Transmitter
+#include "Wire.h"
 #include "SSD1306Ascii.h"
-#include "SSD1306AsciiAvrI2c.h"
+#include "SSD1306AsciiWire.h"
 
 #define I2C_ADDRESS 0x3C
 
@@ -8,10 +9,10 @@
 const int unitDelay = 250;
 
 // Define pins
-const int buttonPin = 2;
-const int transmissionPin = A5;
-const int ledPin = 13;
-const int buzzer = 3;
+const int buttonPin = 13;
+const int transmissionPin = 12;
+const int ledPin = 8;
+const int buzzer = A0;
 
 // Initial pin states
 int ledState = LOW;
@@ -28,14 +29,16 @@ boolean checker = false;
 boolean linechecker = false;
 
 // Create the display object
-SSD1306AsciiAvrI2c oled;
+SSD1306AsciiWire oled;
 
 void setup() {
+  Wire.begin();
+  Wire.setClock(400000L);
   pinMode(buttonPin, INPUT);
   pinMode(ledPin, OUTPUT);
   pinMode(buzzer, OUTPUT);
   pinMode(transmissionPin, OUTPUT);
-  digitalWrite(ledPin, HIGH);
+  digitalWrite(ledPin, LOW);
   oled.begin(&Adafruit128x64, I2C_ADDRESS);
   oled.setFont(Adafruit5x7);
   oled.setScrollMode(SCROLL_MODE_AUTO);
@@ -84,84 +87,4 @@ void loop() {
   }
   lastButtonState=buttonState;
   delay(1);
-}
-
-void displayChar(String code) { //ugly part of the code but it works fine
-                                //compare morse string to known morse values and print out the letter or a number 
-                                //the code is written based on the international morse code, one thing i changed is that insted of typing a special string to end the line it happens with enough delay  
-  if (code == ".-")
-    oled.print("A");
-  else if (code == "-...")  
-    oled.print("B");
-  else if (code == "-.-.")  
-    oled.print("C");
-  else if (code == "-..")  
-    oled.print("D");
-  else if (code == ".")  
-    oled.print("E");
-  else if (code == "..-.")  
-    oled.print("F");
-  else if (code == "--.")  
-    oled.print("G");
-  else if (code == "....")  
-    oled.print("H");
-  else if (code == "..")  
-    oled.print("I");
-  else if (code == ".---")  
-    oled.print("J");
-  else if (code == "-.-")  
-    oled.print("K");
-  else if (code == ".-..")  
-    oled.print("L");
-  else if (code == "--")  
-    oled.print("M");
-  else if (code == "-.")  
-    oled.print("N");
-  else if (code == "---")  
-    oled.print("O");
-  else if (code == ".--.")  
-    oled.print("P");
-  else if (code == "--.-")  
-    oled.print("Q");
-  else if (code == ".-.")  
-    oled.print("R");
-  else if (code == "...")  
-    oled.print("S");
-  else if (code == "-")  
-    oled.print("T");
-  else if (code == "..-")  
-    oled.print("U");
-  else if (code == "...-")  
-    oled.print("V");
-  else if (code == ".--")  
-    oled.print("W");
-  else if (code == "-..-")  
-    oled.print("X");
-  else if (code == "-.--")  
-    oled.print("Y");
-  else if (code == "--..")  
-    oled.print("Z");
-  else if (code == ".----")  
-    oled.print("1");
-  else if (code == "..---")  
-    oled.print("2");
-  else if (code == "...--")  
-    oled.print("3");
-  else if (code == "....-")  
-    oled.print("4");
-  else if (code == ".....")  
-    oled.print("5");
-  else if (code == "-....")
-    oled.print("6");
-  else if (code == "--...")  
-    oled.print("7");
-  else if (code == "---..")  
-    oled.print("8");
-  else if (code == "----.")  
-    oled.print("9");
-  else if (code == "-----")  
-    oled.print("0");
-
-  oled.print(" ");
-  code=""; 
 }
