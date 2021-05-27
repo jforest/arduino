9cdcc6cd0288e170ea3dc5654378813e011bb614
Joshua C. Forest <josh.forest@rstudio.com>
2021-05-27 15:07:59 -0400

    new geocache idea

---
+++ sensoryOverload/sensoryOverload.ino
@@ -0,0 +1,20 @@
+/*
+ * sensoryOverload
+ * 
+ * Geocache puzzle, with a bunch of sensors, that must be use in a specific order
+ * to open the box.
+ * 
+ * The order is random
+ * 
+ * jforest 5/26/21
+ */
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
