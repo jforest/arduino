b7f02c4990b72f2ffcc918dc1e2869d3305392ab
Joshua C. Forest <josh.forest@rstudio.com>
2021-09-26 15:22:02 -0400

    initial add of cryptoticker

---
+++ cryptoTicker/cryptoTicker.ino
@@ -0,0 +1,27 @@
+/*
+ * cryptoTicker
+ * 
+ * Display crypto values on a e-ink display, updated every X
+ * seconds.  Default to 60 seconds
+ * 
+ * jforest 08/13/2021
+ */
+
+// Import a file that contains the API key, so it's not checked into git
+ 
+void setup() {
+  // Configure e-ink display, wifi, and status led
+  // Define the cryptos to cycle through
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+float getCryptoUSD(String crypto) {
+  // Return the most recent value of crypto in USD
+  return 43999.27
+}
+
+void drawScreen
