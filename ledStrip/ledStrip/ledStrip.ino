c9f5c00ea633212e72b8071a57feaedf4c6aa833
Joshua C. Forest <jforest@sicilia.localdomain>
2021-10-29 14:30:19 -0400

    pulse leds

--- ledStrip/ledStrip/ledStrip.ino
+++ ledStrip/ledStrip/ledStrip.ino
@@ -1,12 +1,13 @@
 #include <FastLED.h>
 
-#define NUM_LEDS 144
+#define NUM_LEDS 288
 #define DATA_PIN 2
 #define COLOR_ORDER GRB
 #define CHIPSET WS2812B
 #define BRIGHTNESS 60
 #define VOLTS 5
 #define MAX_AMPS 500 // mA
+#define BATCH_SIZE 16
 
 CRGB leds[NUM_LEDS];
 
@@ -21,14 +22,21 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  for (int i=0; i<NUM_LEDS; i++) {
+  for (int i=0; i<NUM_LEDS; i+BATCH_SIZE) {
+    pulseLeds(BATCH_SIZE, i);
+  }
+}
+
+void pulseLeds(int batchSize, int startingLED) {
+  // Pulse LEDs from startingLED to startingLED+batchSize
+  for (int i=startingLED; i<(startingLED + batchSize); i++) {
     leds[i] = CRGB::OrangeRed;
     FastLED.show();
     delay(10);
   }
-  for (int i=NUM_LEDS-1; i>=0; i--) {
-    leds[i] = CRGB::Black;
-    FastLED.show();
-    delay(10);
+  for (int i=startingLED; i<(startingLED + batchSize); i++) {
+      leds[i] = CRGB::Black;
+      FastLED.show();
+      delay(10);
+    }
   }
-}
