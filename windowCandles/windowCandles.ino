8060fd68489c06eea50b2a3134c633f7bbf28116
Joshua C. Forest <josh.forest@rstudio.com>
2021-11-24 09:48:11 -0500

    Better color and saturation.  remove unneeded pieces

--- windowCandles/windowCandles.ino
+++ windowCandles/windowCandles.ino
@@ -1,37 +1,36 @@
 #include "FastLED.h"
 
-#if defined(FASTLED_VERSION) && (FASTLED_VERSION < 3001000)
-#warning "Requires FastLED 3.1 or later; check github for latest code."
-#endif
-
 #define NUM_LEDS      3
 #define LED_TYPE   WS2812B
 #define COLOR_ORDER   GRB
 #define DATA_PIN        7
 #define VOLTS          5
 #define MAX_MA       500
-#define BRIGHTNESS 200
 
 CRGB leds[NUM_LEDS];
+uint8_t hue = 41;  // light yellowish color
+uint8_t saturation = 175; // just enough yellow and not white
+uint8_t brightness = 255;
 
 void setup() {
   FastLED.addLeds<LED_TYPE, DATA_PIN, COLOR_ORDER>(leds, NUM_LEDS);
   FastLED.setMaxPowerInVoltsAndMilliamps(VOLTS, MAX_MA);
-  FastLED.setBrightness(BRIGHTNESS);
+  FastLED.setBrightness(brightness);
   FastLED.clear();
   FastLED.show();
 }
 
 void loop() {
-  int randLED = random(3);
-  flickerLEDs(randLED);
+  EVERY_N_MILLISECONDS(10) {
+    int randLED = random(3);
+    flickerLEDs(randLED);
+  }
 }
 
 void flickerLEDs(int randLED) {
   FastLED.clear();
   for (int led = 0; led <= randLED; led++) {
-    leds[led] = CHSV(43, 217, 218);
+    leds[led] = CHSV(hue, saturation, brightness);
   }
   FastLED.show();
-  delay(10);
 }
